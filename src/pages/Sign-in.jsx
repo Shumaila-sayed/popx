5d4513a453aba424d35e94e3e4601c57ebc17d83
Shumaila <sayedshumaila27@gmail.com>
2025-05-23 23:19:32 +0530

    Implement Sign in page

--- src/pages/Sign-in.jsx
+++ src/pages/Sign-in.jsx
@@ -1,5 +1,46 @@
+import Input from '../components/Input';
+import Button from '../components/Button';
+import { NavLink } from 'react-router-dom';
+
 const SignIn = () => {
-	return <div>Sign in</div>;
+	return (
+		<div className='m-10 md:max-w-[18em] md:m-auto '>
+			<div className='md:mt-12'>
+				<h1 className='text-4xl font-bold text-gray-800 mb-5'>
+					Sign in to your PopX account
+				</h1>
+				<p className='text-gray-500 font-semibold text-[22px] mb-5'>
+					Lorem ipsum dolor sit amet, consectetut adipiscing elit
+				</p>
+			</div>
+			<div>
+				<form
+					action=''
+					className='flex flex-col gap-5'
+				>
+					<Input
+						type='email'
+						placeholder='Enter email address'
+						text='Email Address'
+						name='e-mail'
+					/>
+					<Input
+						type='password'
+						placeholder='Enter password'
+						text='Password'
+						name='p-word'
+					/>
+					<NavLink to='/settings'>
+						<Button
+							type='submit'
+							role='secondary'
+							text='Login'
+						/>
+					</NavLink>
+				</form>
+			</div>
+		</div>
+	);
 };
 
 export default SignIn;
